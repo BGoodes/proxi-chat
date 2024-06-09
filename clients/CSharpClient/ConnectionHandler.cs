78a9220d99d8cc4ddeb66986a766acda28ef0ab2
BGoodes <moullecjulienpro@gmail.com>
2024-06-09 11:37:24 +0800

    Add CSharp client library

---
+++ clients/CSharpClient/ConnectionHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace ProxiChatCSharp;
+
+public static class ConnectionHandler
+{
+    public static void HandleConnection(SocketIOClient.SocketIO socket, String gameName)
+    {
+        socket.OnConnected += async (sender, e) =>
+        {
+            Console.WriteLine("Connected to server");
+            await socket.EmitAsync("join", new { userId = gameName, type = "game" });
+        };
+
+        socket.OnDisconnected += (sender, e) =>
+        {
+            Console.WriteLine("Disconnected from server");
+        };
+
+        socket.OnReconnectAttempt += (sender, e) =>
+        {
+            Console.WriteLine($"Reconnecting: attempt {e}");
+        };
+
+        socket.OnError += (sender, e) =>
+        {
+            Console.WriteLine($"Error: {e}");
+        };
+    }
+}
---
+++ clients/CSharpClient/ProxiChatClient.cs
@@ -0,0 +1,34 @@
+﻿namespace ProxiChatCSharp;
+
+public class ProxiChatClient
+{
+    private readonly SocketIOClient.SocketIO _socket;
+
+    public ProxiChatClient(String gameName, String url)
+    {
+        _socket = new SocketIOClient.SocketIO(url);
+        ConnectionHandler.HandleConnection(_socket, gameName);
+    }
+
+    public async Task ConnectAsync()
+    {
+        await _socket.ConnectAsync();
+    }
+
+    public async Task DisconnectAsync()
+    {
+        await _socket.DisconnectAsync();
+    }
+
+    public async Task SendCoordinatesAsync(String userId, int x, int y, int z)
+    {
+        var data = new
+        {
+            userId,
+            coordinates = new { x = x, y = y, z = z}
+        };
+
+        await _socket.EmitAsync("coordinates", data);
+        Console.WriteLine($"Coordinates sent: {data.userId}, {string.Join(", ", data.coordinates)}");
+    }
+}
